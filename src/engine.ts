1b5102e729da9610110aa2082a9e5df1f1651af9
CloudRex <cloudrex@outlook.com>
2019-02-21 09:18:33 -0500

    Begin implementing game loop

--- src/engine.ts
+++ src/engine.ts
@@ -3,15 +3,33 @@ import EngineEvent from "./engine-event";
 import Entity from "./entity";
 import Id from "./id";
 
+export type RenderCallback = (time: number) => void;
+
 export default class Engine extends EventEmitter {
     protected fps: number;
     protected entities: Map<Id, Entity>;
+    protected renderCallback: RenderCallback;
+    protected running: boolean;
 
-    public constructor() {
+    public constructor(renderCallback: RenderCallback) {
         super();
 
         this.fps = 30;
         this.entities = new Map();
+        this.renderCallback = renderCallback;
+        this.running = false;
+    }
+
+    protected render(): void {
+        this.renderCallback();
+    }
+
+    public start(): this {
+        window.requestAnimationFrame((time: number) => {
+
+        });
+
+        return this;
     }
 
     public getFps(): number {
